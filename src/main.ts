7ff245f2ded44974e9f6aa89c28437fb1b43fa4f
Quang Bui <buiminhquang188@gmail.com>
2023-06-06 22:04:29 +0700

    ➕ : Highlightjs & Vue plugin

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import './assets/main.css'
+import 'highlight.js/styles/github-dark.css'
+import 'highlight.js/lib/common'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +12,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(hljsVuePlugin)
 app.use(router)
 
 app.mount('#app')
